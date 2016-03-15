527874a1821135d3341f992ae5c3ad2229c0f7af
kbb29 <kbb29@github.com>
2016-03-15 22:28:01 +0000

    started work on strava upload

--- src/web/tcx2strava.py
+++ src/web/tcx2strava.py
@@ -22,7 +22,7 @@ class StravaCallbackServer(BaseHTTPServer.HTTPServer):
       self.onetimecode = code
 
     def get_onetimecode(self):
-      return self.onetimecode
+      return self.onetimecode 
     
       
 
@@ -31,11 +31,12 @@ class StravaCallbackReceiver(BaseHTTPServer.BaseHTTPRequestHandler):
       print self.path
       
       #regex = re.compile('\/authorized?code=(\w+)')
-      m = re.match('\/authorized\?code=(\w+)',  self.path)
+      m = re.match('\/authorized\?[\w%&=\-]*code=(\w+)',  self.path)
       if(m):
         self.server.set_onetimecode(str(m.group(1)))
         print "match! " + self.server.get_onetimecode()
       else:
+        self.server.set_onetimecode("")
         print "no match " + self.path
 
 
@@ -44,34 +45,33 @@ def main():
     logging.basicConfig(level=logging.DEBUG)
 
     localwebserverport = 8282
+    clientid=10615
 
     client = Client()
-    authorize_url = client.authorization_url(client_id=10615, redirect_uri='http://localhost:'+str(localwebserverport)+'/authorized')
+    authorize_url = client.authorization_url(client_id=clientid, redirect_uri='http://localhost:'+str(localwebserverport)+'/authorized')
     # Have the user click the authorization URL, a 'code' param will be added to the redirect_uri
     # .....
 
-    print authorize_url
+    print( "opening " + authorize_url)
 
-
+    os.system("xdg-open '" + authorize_url + "'")
     
 
     server_address = ('localhost', localwebserverport)
     httpd = StravaCallbackServer(server_address, StravaCallbackReceiver)
+    print("waiting for user...")
     httpd.handle_request()
 
-    print httpd.get_onetimecode()
-
-    return
-    
     # Extract the code from your webapp response
-    code = request.get('code') # or whatever your framework does
-    access_token = client.exchange_code_for_token(client_id=1234, client_secret='asdf1234', code=code)
+    code = httpd.get_onetimecode()
+    access_token = client.exchange_code_for_token(client_id=clientid, client_secret='0d95d8ab7a142820140dada94268e663d66a7b77', code=code)
     
     # Now store that access token somewhere (a database?)
     client.access_token = access_token
     athlete = client.get_athlete()
     print("For {id}, I now have an access token {token}".format(id=athlete.id, token=access_token))
 
+    return
     parser = argparse.ArgumentParser(description='Uploads TCX to the Internets')
     parser.add_argument(nargs='+', dest='tcx', help='TCX files to upload')
     args = parser.parse_args()
